13d60edb602d93933c2aba861667844b4975b486
moloch-- <fierynudist@gmail.com>
2012-08-06 02:20:14 -0700

    Application starts and renders pages correctly

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
-"""
+'''
+    Copyright [2012] [Redacted Labs]
 
- Copyright [2012] [Redacted Labs]
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+'''
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
 
--------
-"""
 import logging
 import tornado.web
+
+
+from libs.ConsoleColors import *
+from libs.Memcache import FileCache
+from libs.ConfigManager import ConfigManager
 from tornado.web import Application
 from models import dbsession
 from modules.Menu import Menu
@@ -28,6 +32,8 @@ from handlers.StaticFileHandler import StaticFileHandler
 from PublicHandlers import *
 from ErrorHandlers import *
 
+
+config = ConfigManager.Instance()
 application = Application([
 
         #Static Handlers - Serves static CSS, JavaScript and image files
@@ -49,20 +55,32 @@ application = Application([
     cookie_secret = b64encode(urandom(64)),
 
     # Debug mode
-    debug = True,
+    debug = config.debug,
+
+    # Enable XSRF forms
+    xsrf_cookies = True,
+
+    # Recaptcha Settings
+    recaptcha_enable = config.recaptcha_enable,
+    recaptcha_private_key = config.recaptcha_private_key,
+
+    # WebSocket Host IP Address
+    ws_ip_address = config.websocket_host,
+    ws_port = config.listen_port,
 
     # UI Modules
     ui_modules={"Menu": Menu},
     
     # Application version
-    version = '0.0.1'
+    version = '0.1'
 )
 
 # Main entry point
 def start_game():
-     try:
-          application.listen(8888)
-          tornado.ioloop.IOLoop.instance().start()
-
-     except KeyboardInterrupt:
-          print "Shutting Down!"
+    try:
+        print("\r" + INFO + "Starting web server; binding to port %d" % config.listen_port)
+        application.listen(config.listen_port)
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        FileCache.flush()
+        print("\r" + WARN + "Shutting everything!")
